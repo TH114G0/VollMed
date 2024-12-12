aacf287ddb5007e8d72550396b40dd0b6c33458b
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-12 16:52:12 -0300

    "Implementa controlador para gerenciamento de médicos"

---
+++ src/main/java/voll/med/api/controller/DoctorController.java
@@ -0,0 +1,40 @@
+package voll.med.api.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import voll.med.api.dto.DoctorDTO;
+import voll.med.api.dto.DoctorListDataDTO;
+import voll.med.api.dto.MedicalUpdateDataDTO;
+import voll.med.api.service.DoctorService;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @PostMapping
+    public void create(@RequestBody @Valid DoctorDTO doctorDTO) {
+        doctorService.create(doctorDTO);
+    }
+
+    @GetMapping
+    public Page<DoctorListDataDTO> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
+        return doctorService.list(pageable).map(DoctorListDataDTO::new);
+    }
+
+    @PutMapping
+    public void update(@RequestBody MedicalUpdateDataDTO medicalUpdateDataDTO) {
+        doctorService.update(medicalUpdateDataDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        doctorService.delete(id);
+    }
+}
