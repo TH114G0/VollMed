e21622ca760455206e8e84c59581582359083e40
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-12 16:47:14 -0300

    Address um Embeddable de Doctors com suas funcionalidades getters, setters, construtor default e seu método de atualizar

---
+++ src/main/java/voll/med/api/entiy/Address.java
@@ -0,0 +1,64 @@
+package voll.med.api.entiy;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import voll.med.api.dto.AddressDTO;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    private String street;
+
+    private String neighborhood;
+
+    private String postalCode;
+
+    private String city;
+
+    private String state;
+
+    private String number;
+
+    private String complement;
+
+    public Address(AddressDTO addressDTO) {
+        this.street = addressDTO.street();
+        this.neighborhood = addressDTO.neighborhood();
+        this.postalCode = addressDTO.postalCode();
+        this.city = addressDTO.city();
+        this.state = addressDTO.state();
+        this.number = addressDTO.number();
+        this.complement = addressDTO.complement();
+    }
+
+    public void update(AddressDTO addressDTO) {
+        if (addressDTO.street() != null) {
+            this.street = addressDTO.street();
+        }
+        if (addressDTO.neighborhood() != null) {
+            this.neighborhood = addressDTO.neighborhood();
+        }
+        if (addressDTO.postalCode() != null) {
+            this.postalCode = addressDTO.postalCode();
+        }
+        if (addressDTO.city() != null) {
+            this.city = addressDTO.city();
+        }
+        if (addressDTO.state() != null) {
+            this.state = addressDTO.state();
+        }
+        if (addressDTO.number() != null) {
+            this.number = addressDTO.number();
+        }
+        if (addressDTO.complement() != null) {
+            this.complement = addressDTO.complement();
+        }
+    }
+}
