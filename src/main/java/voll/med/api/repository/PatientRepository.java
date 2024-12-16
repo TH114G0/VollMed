eb482c422a224f049460fca9ec28e73c388ba372
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-16 17:20:29 -0300

    Criação do repositório de pacientes (PatientRepository)
    - Implementação do repositório PatientRepository utilizando JpaRepository para persistência de dados.
    - Adição do método findAllByActiveTrue para listar pacientes ativos com suporte a paginação.
    - O método retorna uma página de PatientListDataDTO, facilitando a exibição de dados resumidos dos pacientes.

---
+++ src/main/java/voll/med/api/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package voll.med.api.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import voll.med.api.dto.patient.PatientListDataDTO;
+import voll.med.api.entiy.patient.PatientEntity;
+
+public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
+    Page<PatientListDataDTO> findAllByActiveTrue(Pageable pageable);
+}
