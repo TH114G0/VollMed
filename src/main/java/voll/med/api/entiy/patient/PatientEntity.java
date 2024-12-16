dddae6174d2107d2e261b295e39e365aff94a15b
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-16 17:19:52 -0300

    Criação da entidade de paciente (PatientEntity)
    - Implementação da entidade PatientEntity para mapeamento do banco de dados com JPA.
    - Atributos: id, nome, e-mail, CPF, endereço e status de ativo (active).
    - Construtor que converte PatientDTO em PatientEntity para criação de pacientes.
    - Método de atualização (update) para atualizar dados do paciente a partir de PatientUpdateDataDTO.
    - Método de exclusão (delete) para desativar o paciente, marcando o campo active como false.
    - Utilização de @Embedded para mapear o endereço (Address) associado ao paciente.

---
+++ src/main/java/voll/med/api/entiy/patient/PatientEntity.java
@@ -0,0 +1,54 @@
+package voll.med.api.entiy.patient;
+
+import jakarta.persistence.*;
+import lombok.*;
+import voll.med.api.dto.patient.PatientDTO;
+import voll.med.api.dto.patient.PatientUpdateDataDTO;
+import voll.med.api.entiy.address.Address;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class PatientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    @Embedded
+    private Address address;
+
+    private Boolean active;
+
+    public PatientEntity(PatientDTO patientDTO) {
+        this.name = patientDTO.name();
+        this.email = patientDTO.email();
+        this.cpf = patientDTO.cpf();
+        this.address = new Address(patientDTO.addressDTO());
+        this.active = true;
+    }
+
+    public void update(PatientUpdateDataDTO patientUpdateDataDTO) {
+        if(patientUpdateDataDTO.name() != null) {
+            this.name = patientUpdateDataDTO.name();
+        }
+
+        if(patientUpdateDataDTO.addressDTO() != null) {
+            this.address.update(patientUpdateDataDTO.addressDTO());
+        }
+    }
+
+    public void delete() {
+        this.active = false;
+    }
+}
