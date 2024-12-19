c303880c8ca708c5e56b299f433047c038b205f9
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-19 16:58:41 -0300

    feat: implementar serviço de paciente com operações CRUD

--- src/main/java/voll/med/api/service/PatientService.java
+++ src/main/java/voll/med/api/service/PatientService.java
@@ -5,24 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import voll.med.api.domain.patient.PatientEntity;
 import voll.med.api.dto.patient.PatientDTO;
 import voll.med.api.dto.patient.PatientListDataDTO;
 import voll.med.api.dto.patient.PatientUpdateDataDTO;
-import voll.med.api.entiy.patient.PatientEntity;
+import voll.med.api.dto.patient.UpdatePatientDataDTO;
+import voll.med.api.exception.PatientNotFoundException;
 import voll.med.api.repository.PatientRepository;
-
-import java.util.Optional;
 
 @Service
 public class PatientService {
 
-
     @Autowired
     private PatientRepository patientRepository;
 
-    @Transactional
-    public void create(PatientDTO patientDTO) {
-        patientRepository.save(new PatientEntity(patientDTO));
+    public PatientEntity create(PatientDTO patientDTO) {
+        return patientRepository.save(new PatientEntity(patientDTO));
     }
 
     public Page<PatientListDataDTO> list(Pageable pageable) {
@@ -30,24 +28,23 @@ public class PatientService {
     }
 
     @Transactional
-    public void update(PatientUpdateDataDTO patientUpdateDataDTO) {
-        Optional<PatientEntity> optionalPatientEntity = patientRepository.findById(patientUpdateDataDTO.id());
-
-        if(optionalPatientEntity.isPresent()) {
-            PatientEntity patientEntity = optionalPatientEntity.get();
-            patientEntity.update(patientUpdateDataDTO);
-        }
+    public UpdatePatientDataDTO update(PatientUpdateDataDTO patientUpdateDataDTO) {
+        var patientEntity = patientRepository.findById(patientUpdateDataDTO.id())
+                .orElseThrow(() -> new PatientNotFoundException("ID - " + patientUpdateDataDTO.id() + " não encontrado"));
+        patientEntity.update(patientUpdateDataDTO);
+        return new UpdatePatientDataDTO(patientEntity);
     }
 
     @Transactional
     public void delete(Long id) {
-        Optional<PatientEntity> optionalPatientEntity = patientRepository.findById(id);
+        var patientEntity = patientRepository.findById(id)
+                .orElseThrow(() -> new PatientNotFoundException("ID - " + id + " não encontrado"));
+        patientEntity.delete();
+    }
 
-        if(optionalPatientEntity.isPresent()) {
-            PatientEntity patientEntity = optionalPatientEntity.get();
-            patientEntity.delete();
-        }else {
-            throw new RuntimeException("ID - " + id + ", não encontrado.");
-        }
+    public UpdatePatientDataDTO details(Long id) {
+        var patientEntity = patientRepository.findById(id)
+                .orElseThrow(() -> new PatientNotFoundException("ID - " + id + " não encontrado"));
+        return new UpdatePatientDataDTO(patientEntity);
     }
 }
