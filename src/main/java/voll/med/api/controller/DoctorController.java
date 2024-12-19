cfaa1d8085e51760901a3c5aee95276ed13d3f34
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-19 16:49:10 -0300

    - Atualizados os métodos do DoctorController para retornar ResponseEntity - Implementado endpoint GET /doctors/{id} para buscar detalhes de um médico - Adicionada resposta HTTP 201 (Created) no método POST /doctors, incluindo o retorno do objeto criado - Melhorada padronização de respostas para incluir status HTTP adequados:   - GET retorna 200 (OK)   - POST retorna 201 (Created)   - DELETE retorna 204 (No Content) - Incluída classe DTO `UpdateDoctorDataDTO` para formatar as respostas com dados atualizados do médico - Objetivo: aprimorar as respostas da API para torná-las mais informativas e alinhadas com boas práticas REST

--- src/main/java/voll/med/api/controller/DoctorController.java
+++ src/main/java/voll/med/api/controller/DoctorController.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import voll.med.api.dto.doctor.DoctorDTO;
 import voll.med.api.dto.doctor.DoctorListDataDTO;
 import voll.med.api.dto.doctor.MedicalUpdateDataDTO;
 import voll.med.api.service.DoctorService;
+import voll.med.api.dto.doctor.UpdateDoctorDataDTO;
 
 @RestController
 @RequestMapping("/doctors")
@@ -19,22 +23,32 @@ public class DoctorController {
     private DoctorService doctorService;
 
     @PostMapping
-    public void create(@RequestBody @Valid DoctorDTO doctorDTO) {
-        doctorService.create(doctorDTO);
+    public ResponseEntity<UpdateDoctorDataDTO> create(@RequestBody @Valid DoctorDTO doctorDTO, UriComponentsBuilder uriComponentsBuilder) {
+        var doctor = doctorService.create(doctorDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UpdateDoctorDataDTO(doctor));
     }
 
     @GetMapping
-    public Page<DoctorListDataDTO> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
-        return doctorService.list(pageable).map(DoctorListDataDTO::new);
+    public ResponseEntity<Page<DoctorListDataDTO>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
+        var page = doctorService.list(pageable).map(DoctorListDataDTO::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
-    public void update(@RequestBody MedicalUpdateDataDTO medicalUpdateDataDTO) {
-        doctorService.update(medicalUpdateDataDTO);
+    public ResponseEntity<UpdateDoctorDataDTO> update(@RequestBody MedicalUpdateDataDTO medicalUpdateDataDTO) {
+        var doctor = doctorService.update(medicalUpdateDataDTO);
+        return ResponseEntity.ok(doctor);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
         doctorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UpdateDoctorDataDTO> details(@PathVariable Long id) {
+        var doctor = doctorService.details(id);
+        return ResponseEntity.ok(doctor);
     }
 }
