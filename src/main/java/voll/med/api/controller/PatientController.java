0815f851e1d913c9b13d4c051e006a2eb7cd134d
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-19 16:57:39 -0300

    - Alterado o método `create` para retornar o DTO `UpdatePatientDataDTO` com os dados do paciente criado - Atualizado o método `list` para retornar uma página de pacientes com a estrutura `PatientListDataDTO` - Modificado o método `update` para retornar o DTO atualizado do paciente após a atualização - Atualizado o método `delete` para retornar uma resposta 204 (No Content) após a exclusão do paciente - Adicionado o método `details` para buscar os detalhes de um paciente e retornar um DTO com os dados completos - Objetivo: melhorar a comunicação com o cliente da API, retornando dados no formato DTO e tratando respostas HTTP de forma mais adequada

--- src/main/java/voll/med/api/controller/PatientController.java
+++ src/main/java/voll/med/api/controller/PatientController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import voll.med.api.dto.patient.PatientListDataDTO;
 import voll.med.api.dto.patient.PatientDTO;
+import voll.med.api.dto.patient.PatientListDataDTO;
 import voll.med.api.dto.patient.PatientUpdateDataDTO;
+import voll.med.api.dto.patient.UpdatePatientDataDTO;
 import voll.med.api.service.PatientService;
 
 @RestController
@@ -19,22 +22,32 @@ public class PatientController {
     private PatientService patientService;
 
     @PostMapping
-    public void create(@RequestBody @Valid PatientDTO patientDTO) {
-        patientService.create(patientDTO);
+    public ResponseEntity<UpdatePatientDataDTO> create(@RequestBody @Valid PatientDTO patientDTO) {
+        var patient = patientService.create(patientDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UpdatePatientDataDTO(patient));
     }
 
     @GetMapping
-    public Page<PatientListDataDTO> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
-        return patientService.list(pageable).map(PatientListDataDTO::new);
+    public ResponseEntity<Page<PatientListDataDTO>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
+        var page = patientService.list(pageable).map(PatientListDataDTO::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
-    public void update(@RequestBody PatientUpdateDataDTO patientUpdateDataDTO) {
-        patientService.update(patientUpdateDataDTO);
+    public ResponseEntity<UpdatePatientDataDTO> update(@RequestBody PatientUpdateDataDTO patientUpdateDataDTO) {
+        var patient = patientService.update(patientUpdateDataDTO);
+        return ResponseEntity.ok(patient);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
         patientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UpdatePatientDataDTO> details(@PathVariable Long id) {
+        var patient = patientService.details(id);
+        return ResponseEntity.ok(patient);
     }
 }
