993a46c136e2356ef07065311b8ea982470cb8ed
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-19 16:52:40 -0300

    - Alterando nome do pacote "entity" para "domain".

--- src/main/java/voll/med/api/dto/doctor/DoctorListDataDTO.java
+++ src/main/java/voll/med/api/dto/doctor/DoctorListDataDTO.java
@@ -1,6 +1,6 @@
 package voll.med.api.dto.doctor;
 
-import voll.med.api.entiy.doctor.Specialty;
+import voll.med.api.domain.doctor.Specialty;
 
 public record DoctorListDataDTO(String name, String email, String phoneNumber, Specialty specialty) {
     public DoctorListDataDTO(DoctorListDataDTO doctorListDataDTO) {
