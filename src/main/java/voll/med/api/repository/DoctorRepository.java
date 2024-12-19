791cedf49791b19a8c3ff5613a679aaf27c91fa3
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-19 16:54:09 -0300

    - Alterando nome do pacote "entity" para "domain".

--- src/main/java/voll/med/api/repository/DoctorRepository.java
+++ src/main/java/voll/med/api/repository/DoctorRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import voll.med.api.dto.doctor.DoctorListDataDTO;
-import voll.med.api.entiy.doctor.DoctorEntity;
+import voll.med.api.domain.doctor.DoctorEntity;
 
 
 
--- src/main/java/voll/med/api/repository/PatientRepository.java
+++ src/main/java/voll/med/api/repository/PatientRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import voll.med.api.dto.patient.PatientListDataDTO;
-import voll.med.api.entiy.patient.PatientEntity;
+import voll.med.api.domain.patient.PatientEntity;
 
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
     Page<PatientListDataDTO> findAllByActiveTrue(Pageable pageable);
