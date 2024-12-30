424533a3e121d80f3595dff6159e35a6a98e4643
Thiago Jose <thiago.wilton2704@gmail.com>
2024-12-30 15:16:58 -0300

    feat: Adicionar classe GlobalExceptionHandler para tratamento centralizado de exceções
    - Implementada a classe GlobalExceptionHandler para centralizar o tratamento de exceções na aplicação.
    - A classe proporciona uma maneira padronizada e eficiente de lidar com erros e enviar respostas adequadas para o cliente.
    - A inclusão dessa classe visa melhorar a manutenibilidade do código e tornar o gerenciamento de exceções mais consistente em todo o sistema.

--- src/main/java/voll/med/api/exception/GlobalExceptionHandler.java
+++ src/main/java/voll/med/api/exception/GlobalExceptionHandler.java
@@ -1,19 +1,22 @@
 package voll.med.api.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(PatientNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handlePatientNotFoundException(PatientNotFoundException ex) {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(EntityNotFoundException ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("timestamp", LocalDateTime.now());
         response.put("status", HttpStatus.NOT_FOUND.value());
@@ -22,13 +25,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(DoctorNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleDoctorNotFoundException(DoctorNotFoundException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.NOT_FOUND.value());
-        response.put("error","Not Found");
-        response.put("message", ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    public record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
     }
 }
